ea2106db9a2c97b8548bc9a17179eaa45b97f7bc
Hemanth Kumar Makkena <hemanth63049@gmail.com>
2025-02-01 16:39:01 +0530

    Delete Triangle.py

--- Triangle.py
+++
@@ -1,7 +0,0 @@
-from FigMenu import menu
-def tarea():
-    print("Triangle")
-    b=float(input("Enter base value:"))
-    h=float(input("Enter height value:"))
-    ta=0.5*b*h
-    print("Area of Rectangle=",ta)
