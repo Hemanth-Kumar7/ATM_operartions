d0c26b80a794dd2ff4bca9f71ed53dcb19968d45
Hemanth Kumar Makkena <hemanth63049@gmail.com>
2025-02-01 16:41:15 +0530

    Delete Rectangle.py

--- Rectangle.py
+++
@@ -1,7 +0,0 @@
-from FigMenu import menu
-def rarea():
-    print("Rectangle")
-    l=float(input("Enter length:"))
-    b=float(input("Enter breadth:"))
-    ra=l*b
-    print("Area of Rectangle =",ra)
