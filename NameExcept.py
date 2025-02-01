4c12ecd5cf49e69c8144d23718d57dade478fdbc
Hemanth Kumar Makkena <hemanth63049@gmail.com>
2025-02-01 16:38:31 +0530

    Delete NameExcept.py

--- NameExcept.py
+++
@@ -1,4 +0,0 @@
-#NameExcept.py    <-------- File and Module Name
-class InvalidNameError(Exception):pass   #InvalidName
-class EmptyStrError(BaseException):pass  #""
-class SpaceError(Exception):pass
